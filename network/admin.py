486aa88e753338f698ef22dfe62259bee635782b
Nathaniel Berube <Rexar14@gmail.com>
2023-01-17 15:47:33 -0600

    Created WIP admin views

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from network.models import User, New_Post
 
 # Register your models here.
+
+
+admin.site.register(User)
+
+# TODO: Eventually create CLASS ManyToManyInline(admin.TabularInline)
+
+
+class NewPostAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "comment_field",
+    )
+
+    # inlines =[]
+
+    # exclude = (said inlines)
+
+
+admin.site.register(New_Post, NewPostAdmin)
