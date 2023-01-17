43383a2be6d6bf19910084aa547d036933bd296a
Nathaniel Berube <Rexar14@gmail.com>
2023-01-17 00:33:20 -0600

    Created new_post model

--- network/models.py
+++ network/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+# Class for New Form (possibly add mmore models or less models not sure yet)
+class New_Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    comment_field = models.TextField(
+        max_length=1000,
+        help_text="Create a post! (1000 character limit!)",
+        blank=False,
+    )
