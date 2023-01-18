171c266d7576a51485bd7ebd0aa7c51c8e58dfff
Nathaniel Berube <Rexar14@gmail.com>
2023-01-18 14:39:29 -0600

    Added timezones to user registration

--- network/views.py
+++ network/views.py
@@ -1,12 +1,13 @@
+import pytz
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from .models import User, New_Post
 from .forms import new_post_form
+from .models import New_Post, User
 
 
 def index(request):
@@ -41,30 +42,46 @@ def logout_view(request):
 
 
 def register(request):
+    timezones = pytz.common_timezones
     if request.method == "POST":
         username = request.POST["username"]
         email = request.POST["email"]
+        timezone = request.POST["timezone"]
 
         # Ensure password matches confirmation
         password = request.POST["password"]
         confirmation = request.POST["confirmation"]
         if password != confirmation:
             return render(
-                request, "network/register.html", {"message": "Passwords must match."}
+                request,
+                "network/register.html",
+                {"message": "Passwords must match.", "timezones": timezones},
             )
 
         # Attempt to create new user
         try:
-            user = User.objects.create_user(username, email, password)
+            user = User.objects.create_user(username, email, password)  # type: ignore
+            tz = pytz.timezone(timezone)
+            user.timezone = tz
             user.save()
-        except IntegrityError:
+        except IntegrityError as ie:
             return render(
-                request, "network/register.html", {"message": "Username already taken."}
+                request,
+                "network/register.html",
+                {"message": "Username already taken.", "timezones": timezones},
             )
+
+        except ValueError as ve:
+            return render(
+                request,
+                "network/register.html",
+                {"message": "No information was entered", "timezones": timezones},
+            )
+
         login(request, user)
         return HttpResponseRedirect(reverse("index"))
     else:
-        return render(request, "network/register.html")
+        return render(request, "network/register.html", {"timezones": timezones})
 
 
 @login_required(redirect_field_name="", login_url="login")
