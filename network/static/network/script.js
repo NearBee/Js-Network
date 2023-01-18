ba3c7ea92bf44f8cca2cf005a5b173889b1512ff
Nathaniel Berube <Rexar14@gmail.com>
2023-01-18 16:17:51 -0600

    script notes

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function like_button() {
-    alert("I've been clicked!")
+    alert("I've been clicked!");
 }
-// May have to use fetch(), which would require making an mock API request 
+// May have to use fetch(), which would require making an mock API request
+// Look at Lecture 5 notes "Counter" for an idea of what to possibly do
+// Maybe do something like "likes++" if button === "like"
