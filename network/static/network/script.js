4a3a4ab42d70903ee6ef3ec1e474ebfc710004cf
Nathaniel Berube <Rexar14@gmail.com>
2023-01-21 01:12:18 -0600

    made JS for following/unfollowing

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -1,13 +1,48 @@
 document.addEventListener('DOMContentLoaded', function () {
     const likeButtons = document.querySelectorAll('#likeButton');
+    const followButtons = document.querySelectorAll(".followButton");
+
     for (let button of likeButtons) {
         button.addEventListener('click', like_button);
     }
+    if (followButtons) {
+        console.log("follow button found");
+        for (let button of followButtons) {
+            button.addEventListener("click", function () {
+                let username = button.getAttribute("data-username");
+                follow_button(username);
+            });
+        }
+    } else {
+        console.log("follow button not found");
+    }
 });
+
 
 function like_button() {
     alert("I've been clicked!");
 }
+
+function follow_button(username) {
+
+    fetch(`/follow_unfollow/${username}`, {
+        method: 'POST',
+        body: JSON.stringify({
+            username: username
+        })
+    })
+        .then((response) => {
+            if (document.querySelector(".followButton").contains("Follow")) {
+                document.querySelector(".followButton").innerHTML = `<button class="followButton" data-username="${username}">Unfollow</button>`;
+            } else {
+                document.querySelector(".followButton").innerHTML = `<button class="followButton" data-username="${username}">Follow</button>`;
+            }
+        })
+        .catch(error => {
+            console.log(`Error:${error}`);
+        });
+}
+
 // May have to use fetch(), which would require making an mock API request
 // Look at Lecture 5 notes "Counter" for an idea of what to possibly do
 // Maybe do something like "likes++" if button === "like"
