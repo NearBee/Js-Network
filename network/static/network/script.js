a08b8890008671e0c50e45bf861372f510e80fac
Nathaniel Berube <Rexar14@gmail.com>
2023-01-21 19:41:17 -0600

    added a script for liking a post

--- network/static/network/script.js
+++ network/static/network/script.js
@@ -2,25 +2,53 @@ document.addEventListener('DOMContentLoaded', function () {
     const likeButtons = document.querySelectorAll('#likeButton');
     const followButtons = document.querySelectorAll("#followButton");
 
+    // Finding and utilizing Like Button
     for (let button of likeButtons) {
-        button.addEventListener('click', like_button);
+        button.addEventListener('click', function () {
+            let id = button.getAttribute("data-id");
+            like_button(id);
+        });
     }
-    if (followButtons) {
-        console.log("follow button found");
-        for (let button of followButtons) {
-            button.addEventListener("click", function () {
-                let username = button.getAttribute("data-username");
-                follow_button(username);
-            });
-        }
-    } else {
-        console.log("follow button not found");
+
+    // Finding and utilizing Follow Button
+    for (let button of followButtons) {
+        button.addEventListener("click", function () {
+            let username = button.getAttribute("data-username");
+            follow_button(username);
+        });
     }
 });
 
 
-function like_button() {
-    alert("I've been clicked!");
+function like_button(id) {
+    fetch(`/like_post/${id}`, {
+        method: 'POST',
+        body: JSON.stringify({
+            id: id
+        })
+    })
+        .then(() => {
+            fetch(`/get_likes/${id}`)
+                .then(response => response.json())
+                .then(data => {
+                    const likes = data.likes;
+                    document.querySelector(`#post-${id} #current_likes`).innerHTML = `Likes: ${likes}`;
+                })
+                .catch(error => {
+                    console.log(`Error: ${error}`);
+                })
+        })
+        .then((response => {
+            if (document.querySelector("#likeButton").contains("Follow")) {
+                // TODO: Doesn't currently work fix this so that the innerHTML
+                document.querySelector("#likeButton").innerHTML = `<button id="likeButton" class="btn btn-outline-danger btn-sm" data-id="${id}">Unfollow</button>`;
+            } else {
+                document.querySelector("#likeButton").innerHTML = `<button id="likeButton" class="btn btn-outline-primary btn-sm" data-id="${id}">Follow</button>`;
+            }
+        }))
+        .catch(error => {
+            console.log(`Error: ${error}`);
+        })
 }
 
 function follow_button(username) {
