559774324f12b68641424605d0674897b7561524
Nathaniel Berube <Rexar14@gmail.com>
2023-01-21 19:41:31 -0600

    added likes M2M to new_post

--- network/models.py
+++ network/models.py
@@ -22,9 +22,7 @@ class New_Post(models.Model):
         max_length=1000,
         blank=False,
     )
-    likes = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="like", blank=True, null=True
-    )
+    likes = models.ManyToManyField(User, related_name="like", blank=True, null=True)
     created_at = models.DateTimeField(
         auto_now_add=True,
     )
